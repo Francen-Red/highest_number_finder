74d5e16336185367fee7c8f45652ad3b32b159c0
Francen-Red <francenvenissered@gmail.com>
2024-10-14 16:20:13 +0800

    Implemented highest_number function to find the highest number of five inputs

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -1,30 +1,33 @@
 # Define highest_number
-
-# Make a variable "highest" and assume that the first input number is the highest
+def highest_number(num1, num2, num3, num4, num5):
+# Make a variable "highest"
+    highest = num1  # Assume that the first input number is the highest
 
 # Compare num2 to "highest"
-# If num2 is greater than "highest"
-    # Update "highest"
-    # If not, do not change "highest"
+    if num2 > highest:  # If num2 is greater than "highest"
+        highest = num2  # Update "highest"
+    else:
+        highest = highest  # If not, do not change "highest"
 
+    # Compare num3 to "highest"
+    if num3 > highest:  # If num3 is greater than "highest"
+        highest = num3  # Update "highest"
+    else:
+        highest = highest  # If not, do not change "highest"
 
-# Compare num3 to "highest"
-# If num3 is greater than "highest"
-    # Update "highest"
-    # If not, do not change "highest"
+    # Compare num4 to "highest"
+    if num4 > highest:  # If num4 is greater than "highest"
+        highest = num4  # Update "highest"
+    else:
+        highest = highest  # If not, do not change "highest"
 
-# Compare num4 ato "highest"
-# If num4 is greater than "highest"
-    # Update "highest"
-    # If not, do not change "highest"
+    # Compare num5 to "highest"
+    if num5 > highest:  # If num5 is greater than "highest"
+        highest = num5  # Update "highest"
+    else:
+        highest = highest  # If not, do not change "highest"
 
-
-# Compare num5 to "highest"
-# If num5 is greater than "highest"
-    # Update "highest"
-    # If not, do not change "highest"
-
-# Return "highest"
+    return highest  # Return the highest number
 
 
 # Ask the user to input five number
@@ -32,4 +35,3 @@
 # Store the value returned in the variable "result"
 
 # Print the result
-
