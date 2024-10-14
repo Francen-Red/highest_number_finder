1dc20e037cb0dd8feb4df406d591e0a725afb28c
Francen-Red <francenvenissered@gmail.com>
2024-10-14 16:49:46 +0800

    Removed else statements

--- highest_number_finder.py
+++ highest_number_finder.py
@@ -6,26 +6,22 @@ def highest_number(num1, num2, num3, num4, num5):
 # Compare num2 to "highest"
     if num2 > highest:  # If num2 is greater than "highest"
         highest = num2  # Update "highest"
-    else:
-        highest = highest  # If not, do not change "highest"
+ # If not, do not change "highest"
 
     # Compare num3 to "highest"
     if num3 > highest:  # If num3 is greater than "highest"
         highest = num3  # Update "highest"
-    else:
-        highest = highest  # If not, do not change "highest"
+# If not, do not change "highest"
 
     # Compare num4 to "highest"
     if num4 > highest:  # If num4 is greater than "highest"
         highest = num4  # Update "highest"
-    else:
-        highest = highest  # If not, do not change "highest"
+ # If not, do not change "highest"
 
     # Compare num5 to "highest"
     if num5 > highest:  # If num5 is greater than "highest"
         highest = num5  # Update "highest"
-    else:
-        highest = highest  # If not, do not change "highest"
+ # If not, do not change "highest"
 
     return highest  # Return the highest number
 
