5c215f21d82c99c4214ff5544e27f12bd7a3c038
Francen-Red <francenvenissered@gmail.com>
2024-10-14 14:36:02 +0800

    Pseudocode for the program

---
+++ highest_number_finder.py
@@ -0,0 +1,35 @@
+# Define highest_number
+
+# Make a variable "highest" and assume that the first input number is the highest
+
+# Compare num2 to "highest"
+# If num2 is greater than "highest"
+    # Update "highest"
+    # If not, do not change "highest"
+
+
+# Compare num3 to "highest"
+# If num3 is greater than "highest"
+    # Update "highest"
+    # If not, do not change "highest"
+
+# Compare num4 ato "highest"
+# If num4 is greater than "highest"
+    # Update "highest"
+    # If not, do not change "highest"
+
+
+# Compare num5 to "highest"
+# If num5 is greater than "highest"
+    # Update "highest"
+    # If not, do not change "highest"
+
+# Return "highest"
+
+
+# Ask the user to input five number
+
+# Store the value returned in the variable "result"
+
+# Print the result
+
